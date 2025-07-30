d3908e9222b046b683695d7cf3bf4d20e5bcd95e
Ahmad Zaki <ahmadzacky46@gmail.com>
2025-07-30 16:42:21 +0700

    membuat kerangka web

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import { useState } from 'react'
-import './App.css'
+import "./App.css"
 
-const App = () => {
-  return (
-    <>
-    
-   
-    </>
+function App (){
+  return(
+  <div>
+    <h3 className="text-4xl text-red-600 font-bold">Hello World</h3>
+  </div>
   )
- 
 }
-
 
 export default App
